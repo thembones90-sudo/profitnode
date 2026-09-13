961c043e11eb00ec4db4c9aa7ffd0e14b0ab0f77
thembones90-sudo <thembones90@gmail.com>
2026-09-13 14:12:31 +0200

    perf: debounce full-page render on rig editor / planner keystrokes

--- pn_scripts.js
+++ pn_scripts.js
@@ -1,6 +1,6 @@
 "use strict";
 window.__PN_SCRIPTS = [
-  "app_core.js?v=rig-editor-rating-above-name-v1",
+  "app_core.js?v=render-debounce-v1",
   "cpu_revaluation_extension.js?v=v3-stack-20260912b",
   "gpu_revaluation_extension.js?v=v3-stack-20260912b",
   "motherboard_revaluation_extension.js?v=v3-stack-20260912b",
