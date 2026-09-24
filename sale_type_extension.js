6c91510733b20e0c9f9d692451bfca84197f07e1
thembones90-sudo <thembones90@gmail.com>
2026-09-25 01:02:55 +0200

    Fix gift badge image asset rendering

--- sale_type_extension.js
+++ sale_type_extension.js
@@ -121,7 +121,7 @@ function saleSummary(currency){
 }
 
 function giftBadgeImageHtml(){
-  return '<span class="pn-gift-badge-image" aria-label="GIFT" title="Gifted rig"></span>';
+  return '<img class="pn-gift-badge-image" src="gift_badge_ui.png?v=gift-present-v2" alt="GIFT" title="Gifted rig">';
 }
 
 function saleFilterButton(label,value,current){
